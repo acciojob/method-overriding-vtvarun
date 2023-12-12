3ab41b882cc230c95cc3c6f14dc878a6e11fd80b
Varun Tanwar <newvaruntanwar@gmail.com>
2023-12-12 12:45:52 +0530

    Method-Overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
+
+
 package com.driver;
 
+class A{
+    public String meth(){
+        return "This is from Class A";
+    }
+}
+
+class B extends A{
+    public String meth(){
+        return "This is from Class B";
+    }
+}
+
 public class Main {
-  
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.meth());
+    }
 }
