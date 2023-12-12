04c2e6654644274f52c6333ee2bc6b18a80c6c5b
Varun Tanwar <newvaruntanwar@gmail.com>
2023-12-12 13:02:28 +0530

    MethodOverrding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,5 +16,8 @@ public class Main {
     public static void main(String[] args) {
         B b = new B();
         System.out.println(b.meth());
+
+        B b1 = new B();
+        System.out.println(b.meth());
     }
 }
